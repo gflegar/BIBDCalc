61bfb06a0cae75247b117fd3554adc6ecb3d1f57
Goran Flegar <flegar.goran@gmail.com>
2015-04-06 13:21:19 +0200

    Add check for validity of BIBDParams.

--- src/bibdcalc/_bibdparams.py
+++ src/bibdcalc/_bibdparams.py
@@ -1,5 +1,6 @@
 
 from . import _bibderrors
+import math
 
 class BIBDParams(object):
     """A class representing BIBD parameters"""
@@ -12,15 +13,17 @@ class BIBDParams(object):
         self.lambda_ = lambda_
     
     def __repr__(self):
-        return "BIBDParams({t}, {v}, {k}, {lambda})".format(**self.getParams())
+        return ("BIBDParams({t}, {v}, {k}, {lambda})".
+            format(**self.get_params()))
 
     def __str__(self):
-        return "{t}-({v},{k},{lambda})".format(**self.getParams())
+        return ("{t}-({v},{k},{lambda})".
+            format(**self.get_params()))
 
-    def getParams(self):
+    def get_params(self):
         return {"t": self.t, "v": self.v, "k": self.k, "lambda":self.lambda_}
 
-    def getFullParams(self):
+    def get_full_params(self):
         r, b = None, None
         for params in self.reduced_t():
             r, b = b, params.lambda_
@@ -40,3 +43,21 @@ class BIBDParams(object):
                                      current.k, lambda_t)
         yield current
 
+    def ray_chavdhuri_wilson_test(self, params = None):
+        if params is None:
+            params = self.get_full_params()
+        v, th, k, b = params['v'], params['t'] // 2, params['k'], params['b']
+        return (v < k + th or
+                b >= math.factorial(v) // 
+                     math.factorial(th) // math.factorial(v - th))
+                
+
+    __tests = [ray_chavdhuri_wilson_test]
+
+    def is_valid(self):
+        try:
+            params = self.get_full_params()
+        except _bibderrors.BIBDParamsError:
+            return False
+        return all(test(self, params) for test in BIBDParams.__tests)
+    
